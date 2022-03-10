6d79e5e1ca3078d437c755478f172d1390c55e08
Lucas Melo <luluco250@gmail.com>
2022-03-10 09:22:26 -0300

    IView.cs file-scoped namespace.

--- WinFormsSampleApp/Services/View/IView.cs
+++ WinFormsSampleApp/Services/View/IView.cs
@@ -1,23 +1,23 @@
-namespace WinFormsSampleApp.Services.View
+namespace WinFormsSampleApp.Services.View;
+
+interface IViewParams<TParams>
 {
-    interface IViewParams<TParams>
-    {
-        TParams Params { get; }
-    }
-
-    interface IViewBase
-    {
-        public string Title => "";
-
-        Task UnloadView();
-    }
-
-    interface IView<TParams> : IViewBase
-    {
-        Task<Control> LoadView(IViewParams<TParams> viewParams);
-    }
-
-    readonly struct NoParams {}
-
-    interface IView : IView<NoParams> {}
+    TParams Params { get; }
 }
+
+interface IViewBase
+{
+    public string Title => "";
+
+    Task UnloadView();
+}
+
+interface IView<TParams> : IViewBase
+{
+    Task<Control> LoadView(IViewParams<TParams> viewParams);
+}
+
+readonly struct NoParams {}
+
+interface IView : IView<NoParams> {}
+
